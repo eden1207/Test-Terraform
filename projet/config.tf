c1706f9a70dcbc848c7d148f4e205b7d546cbaf7
eden1207 <alexis.mauray@gmail.com>
2024-09-18 17:51:43 +0200

    Ajout du workspace staging et de la region aws

--- projet/config.tf
+++ projet/config.tf
@@ -29,7 +29,7 @@ locals {
 provider "genesyscloud" {
   oauthclient_id     = local.oauthclient_id[terraform.workspace]
   oauthclient_secret = local.oauthclient_secret[terraform.workspace]
-  aws_region         = ""
+  aws_region         = var.DEV_AWS_REGION
 }
 
 resource "genesyscloud_routing_wrapupcode" "erreur_numero" {
